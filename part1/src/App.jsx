6ff7e389009ec06b83ee36a3176a4f85bbb65cbc
Maria Eduarda Toneto <dudatoneto29@gmail.com>
2023-11-04 18:31:08 -0300

    code formatting

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,16 +3,18 @@ const Header = (props) => {
     <>
       <h1>{props.course}</h1>
     </>
-  )
-}
+  );
+};
 
 const Part = (props) => {
   return (
     <div>
-      <p>{props.part.name} {props.part.exercises}</p>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 const Content = (props) => {
   return (
@@ -21,35 +23,40 @@ const Content = (props) => {
       <Part part={props.parts[1]} />
       <Part part={props.parts[2]} />
     </>
-  )
-}
+  );
+};
 
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
     </>
-  )
-}
+  );
+};
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
@@ -57,7 +64,7 @@ const App = () => {
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
