ad6b187a60520453fb4406bc3b7408c78aad41e2
Maria Eduarda Toneto <dudatoneto29@gmail.com>
2023-11-11 15:45:34 -0300

    exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -23,9 +23,13 @@ const Persons = ({ persons }) => {
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [personFilter, setPersonFilter] = useState([...persons]);
 
   function handleNameChange(event) {
     console.log(event.target.value);
@@ -35,6 +39,16 @@ const App = () => {
   function handleNumberChange(event) {
     console.log(event.target.value);
     setNewNumber(event.target.value);
+  }
+
+  function handlePersonFilter(event) {
+    console.log(event.target.value);
+
+    const filteredNmaes = persons.filter((person) =>
+      person.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+
+    setPersonFilter(filteredNmaes);
   }
 
   function addPerson(event) {
@@ -49,7 +63,9 @@ const App = () => {
         name: newName,
         number: newNumber,
       };
+
       setPersons(persons.concat(personObject));
+      setPersonFilter(persons);
       setNewName("");
       setNewNumber("");
     }
@@ -58,6 +74,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input name="filter" onChange={handlePersonFilter} />
+      </div>
+      <h3>add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name:{" "}
@@ -76,7 +96,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={personFilter} />
     </div>
   );
 };
