998de6c49fe20becff334d2adf22dc18ea7da7c9
Maria Eduarda Toneto <dudatoneto29@gmail.com>
2024-03-15 10:42:01 -0300

    ex 3.16

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -19,50 +19,65 @@ app.use(
   morgan(":method :url :status :res[content-lenght] :response-time ms :data")
 );
 
-app.get("/info", async (request, response) => {
-  const date = new Date();
-  const result = await Person.find({});
-  response.send(`
+app.get("/info", async (request, response, next) => {
+  try {
+    const date = new Date();
+    const result = await Person.find({});
+    response.send(`
     <div>
       <p>Phonebook has info for ${result.length} people</p>
       <p>${date}</p>
     </div>
   `);
-});
-
-app.get("/api/persons", async (request, response) => {
-  const result = await Person.find({});
-  response.json(result);
-});
-
-app.get("/api/persons/:id", async (request, response) => {
-  const result = await Person.findById(request.params.id);
-
-  if (result) {
-    response.json(result);
-  } else {
-    console.log(`There is no person with the id ${request.params.id}`);
-    response.status(404).end();
+  } catch (err) {
+    console.log("Error fetching persons:");
+    next(err);
   }
 });
 
-app.delete("/api/persons/:id", async (request, response) => {
+app.get("/api/persons", async (request, response, next) => {
+  try {
+    const result = await Person.find({});
+    response.json(result);
+  } catch (err) {
+    console.log("Error fetching persons:");
+    next(err);
+  }
+});
+
+app.get("/api/persons/:id", async (request, response, next) => {
+  try {
+    const result = await Person.findById(request.params.id);
+
+    if (result) {
+      response.json(result);
+    } else {
+      console.log(`There is no person with the id ${request.params.id}`);
+      response.status(404).end();
+    }
+  } catch (err) {
+    console.log(`Error fetching person with id ${request.params.id}:`);
+    next(err);
+  }
+});
+
+app.delete("/api/persons/:id", async (request, response, next) => {
   try {
     const result = await Person.findByIdAndDelete(request.params.id);
     if (result) {
       console.log(`Person with the id ${request.params.id} deleted`);
       response.status(204).end();
     } else {
-      console.log(`There is no person with the id ${id}`);
+      console.log(`There is no person with the id ${request.params.id}`);
       response.status(404).end();
     }
   } catch (err) {
-    console.error("Error deleting person:", err);
-    response.status(500);
+    console.error("Error deleting person:");
+    next(err);
   }
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -77,16 +92,42 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  });
+  try {
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    });
 
-  person.save();
+    person.save();
 
-  console.log(`Added ${person.name} to phonebook`);
-  response.json(person);
+    console.log(`Added ${person.name} to phonebook`);
+    response.json(person);
+  } catch (err) {
+    console.error(`Error creating person ${body.name}:`);
+    next(err);
+  }
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    console.log("*** malformatted id ***");
+    console.error(error.message);
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  console.error(error.message);
+
+  response.status(500).end();
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
