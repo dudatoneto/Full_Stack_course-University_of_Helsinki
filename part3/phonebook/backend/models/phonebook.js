5f4fb33898e4464b60bae02f8a55b19766209643
dudatoneto <79384050+dudatoneto@users.noreply.github.com>
2024-03-22 15:16:35 -0300

    Part 3d (#13)
    * ex 3.19
    
    * ex 3.22

--- part3/phonebook/backend/models/phonebook.js
+++ part3/phonebook/backend/models/phonebook.js
@@ -1,22 +1,34 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const URL = process.env.MONGODB_URI;
+const URL = process.env.MONGODB_URI
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-mongoose.connect(URL);
+mongoose.connect(URL)
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
-
-personSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
   },
-});
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(t) {
+        return /^\d{2,3}-\d+$/.test(t)
+      }
+    }
+  }
+})
 
-module.exports = mongoose.model("Person", personSchema);
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Person', personSchema)
