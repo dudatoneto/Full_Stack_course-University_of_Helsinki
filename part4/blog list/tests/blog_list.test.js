d519d8e5caa9dd227abf619e060208b3d80bbeee
Maria Eduarda Toneto <dudatoneto29@gmail.com>
2024-03-26 15:52:55 -0300

    ex 4.6

--- part4/blog list/tests/blog_list.test.js
+++ part4/blog list/tests/blog_list.test.js
@@ -3,13 +3,12 @@ const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 
 test("dummy returns one", () => {
-  const blogs = [];
-  const result = listHelper.dummy(blogs);
+  const result = listHelper.dummy([]);
   assert.strictEqual(result, 1);
 });
 
 describe("total likes", () => {
-  test("array of many elements", () => {
+  test("list of many elements", () => {
     const blogs = [
       { author: "test1", title: "title1", url: "https://test1.com", likes: 1 },
       { author: "test2", title: "title2", url: "https://test2.com", likes: 2 },
@@ -20,12 +19,11 @@ describe("total likes", () => {
   });
 
   test("empty list", () => {
-    const blogs = [];
-    const result = listHelper.totalLikes(blogs);
+    const result = listHelper.totalLikes([]);
     assert.strictEqual(result, 0);
   });
 
-  test("array of only one element"),
+  test("list of only one element"),
     () => {
       const blogs = [
         {
@@ -38,4 +36,39 @@ describe("total likes", () => {
       const result = listHelper.totalLikes(blogs);
       assert.strictEqual(result, 1);
     };
+});
+
+describe("favorite blog", () => {
+  test("list of many elements", () => {
+    const blogs = [
+      { author: "test1", title: "title1", url: "https://test1.com", likes: 1 },
+      { author: "test2", title: "title2", url: "https://test2.com", likes: 2 },
+      { author: "test3", title: "title3", url: "https://test3.com", likes: 3 },
+    ];
+    const result = listHelper.favoriteBlog(blogs);
+    assert.deepStrictEqual(result, {
+      author: "test3",
+      title: "title3",
+      url: "https://test3.com",
+      likes: 3,
+    });
+  });
+
+  test("empty list", () => {
+    const result = listHelper.favoriteBlog([]);
+    assert.strictEqual(result, "no blogs in the list!");
+  });
+
+  test("list of only one element", () => {
+    const blogs = [
+      { author: "test1", title: "title1", url: "https://test1.com", likes: 1 },
+    ];
+    const result = listHelper.favoriteBlog(blogs);
+    assert.deepStrictEqual(result, {
+      author: "test1",
+      title: "title1",
+      url: "https://test1.com",
+      likes: 1,
+    });
+  });
 });
--- part4/blog list/utils/list_helper.js
+++ part4/blog list/utils/list_helper.js
@@ -3,14 +3,27 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (blogs) => {
-  total = 0;
+  let total = 0;
   blogs.forEach((blog) => {
     total += blog.likes;
   });
   return total;
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length != 0) {
+    let maxLikes = { ...blogs[0] };
+    blogs.forEach((blog) => {
+      if (blog.likes > maxLikes.likes) {
+        maxLikes = { ...blog };
+      }
+    });
+    return maxLikes;
+  } else return "no blogs in the list!";
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
