22ba5a5d55910c80f3e03e79a076aa2e3035f59b
Maria Eduarda Toneto <dudatoneto29@gmail.com>
2024-05-08 09:17:31 -0300

    added get users

--- part4/blog list/controllers/users.js
+++ part4/blog list/controllers/users.js
@@ -2,6 +2,11 @@ const usersRouter = require("express").Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
+usersRouter.get("/", async (request, response) => {
+  const user = await User.find({});
+  response.status(200).json(user);
+});
+
 usersRouter.post("/", async (request, response) => {
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(request.body.password, saltRounds);
